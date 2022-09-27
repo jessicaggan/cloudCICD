4eb2b6cc6fd578c810accff9de119997c56c8bff
Jessicaggan <jessica.ganadhi@binus.ac.id>
2022-09-27 15:02:30 +0700

    Initial Commit

---
+++ cloudCICD/cloudCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudCICDApp.swift
+//  cloudCICD
+//
+//  Created by Jessica Geofanie on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct cloudCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
